66eaab1d8ab5723a952e00c0a4025d2be52a07bb
이정민 <102846582+Leejungmin1119@users.noreply.github.com>
2022-11-25 23:05:35 +0900

    Add files via upload

---
+++ c언어 기말프로젝트/오목(0.1ver)/메인함수 - 복사본.cpp
@@ -0,0 +1,124 @@
+﻿#include <stdio.h>
+#pragma warning (disable : 4996)
+#include <math.h>
+#include <string.h>
+#include <windows.h>
+#include <winCon.h>
+#include <conio.h>
+#include <time.h>
+
+typedef int check;
+//extern-----------------------------------------------------------------
+extern int omog_position_x;
+extern int omog_position_y;
+
+
+
+//extern!!//---funtion()------------------------------------------------
+void print_game();//외부프로그램 (출력함수) 파일에서 불러온 함수.
+void delete_print();
+void print_omog();
+void omog_keyboard();
+void omog_background();
+int move(int x,int y);
+
+
+
+
+struct player//플레이어 이름과 점수
+{
+	char player_name[10] = { 0 };
+	int point = 0;
+};
+
+int playomog()
+{
+	player one, two;
+
+	srand(time(NULL));//매번마다 랜덤으로 값이 나오게 된다. 흑백을 정할때 사용된다.
+
+	int turn_select = (rand()%2);
+
+	//플레이어 정보 입력
+	printf("플레이어 1의 이름을 입력하세요. : ");
+	scanf("%s", &one.player_name);
+
+	printf("플레이어 2의 이름을 입력하세요. : ");
+	scanf("%s", &two.player_name);
+
+	Sleep(100);
+	printf("흑돌은..");
+	for (int a = 1; a <= 3; a++)
+	{
+		printf("%d ",a);
+		Sleep(500);
+	}
+
+	//turn의 값에 따라 흑백이 나뉜다.
+	if (turn_select == 1)
+	{
+		printf("%s 입니다!\n",two.player_name);
+	}
+	else if(turn_select == 0)
+	{
+		printf("%s 입니다!\n", one.player_name);
+	}
+
+	return turn_select;
+}
+/*
+
+오목판을 15*15로 출력한 후 배열로도 15*15로 만든다.
+
+turn 이라는 정수를 만든 후 player1부터 시작.
+
+*turn은 rand()를 이용하여 누가 먼저 할지 정한다.
+* 
+* 착수과정에서 오목 판을 벗어나면 안되고 또한 enter를 누르면 나의 돌이 착수가 되며 배열에 저장한다.
+*배열에 저장될때 만약 오목이 되면 게임을 이겼다는 결과창을 출력한 뒤 게임을 더 할건지 물어 오목게임을 반복한다. 
+
+
+
+
+*/
+
+int main(void)//메인 함수
+{
+
+	int turn = 1;
+
+	print_game();
+	int omog_arr_xy[15][15] ={0};//오목판을 숫자로 나타낸다. 착수시 자리 위치랑 오목 판별등에 사용된다.
+	
+	char game_start = getch();
+	delete_print();
+
+	//사용자의 정보를 입력한다.
+	bool turn_check = playomog();
+
+	Sleep(500);
+	printf("\n\n게임을 시작합니다. 아무키나 눌러주세요..\n");
+
+	game_start = getch();
+
+
+	delete_print();
+
+	//게임시작!
+	print_omog();//오목판을 깔아줌
+
+	omog_background();//배경을 깔아줌
+
+
+	// 착수 과정
+
+	omog_keyboard();//착수할때 사용하는 함수.
+
+
+
+
+
+}
+
+
+
---
+++ c언어 기말프로젝트/오목(0.1ver)/출력함수 - 복사본.cpp
@@ -0,0 +1,189 @@
+﻿#include <stdio.h>
+#pragma warning (disable : 4996)
+#include <math.h>
+#include <string.h>
+#include <windows.h>
+#include <winCon.h>
+#include <time.h>
+#include <conio.h>
+
+
+unsigned long dw;//넓이를 반환 할 곳을 선언.
+bool check_function = false;//특정 함수에 들어갔는지 체그하는 함수.
+
+//---------------------------------------------------------------------
+
+int omog_position_x = 82, omog_position_y = 15;
+
+
+
+
+
+
+int move(int x, int y)//출력하는 위치를 바꿔주는 함수,메인함수에서 불러옴
+{
+	COORD print = { x-1,y-1};//초점을 맞춰 COORD print 에 저장	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), print);
+	return 0;
+}
+
+void print_game()//오목게임을 출력해주는 함수
+{
+	int x = 70, y = 10;//출력할 좌표의 변수 저장.
+	move(x,y);
+	printf("  ■■    ■■■   ■■■   ■ ■     ■■   ■    \n");
+	move(70, 11);
+	printf(" ■  ■   ■  ■       ■■ ■ ■    ■  ■  ■    \n");
+	move(70, 12);
+	printf("  ■■    ■■■       ■   ■ ■     ■■   ■    \n");
+	move(70, 13);
+	printf("   ■       ■                                     \n");
+	move(70, 14);
+	printf(" ■■■   ■■■                         ■■■    \n");
+	move(70, 15);
+	printf("                                         ■  ■    \n");
+	move(70, 16);
+	printf("          ■■■                         ■■■    \n");
+	move(70, 17);
+	printf("              ■                                   \n");
+	move(70, 18);
+	printf("              ■                                   \n");
+	move(70, 19);
+
+	move(69, 23);
+
+	for (int a = 69; a <= 118; a++)// "게임 시작"문장주의에 네모난 틀을 만든다.
+	{
+
+		printf("-");
+
+		if (a == 118)
+		{
+
+			for (int b = 23; b <= 37; b++)
+			{
+				move(118, b);
+				printf("|");
+			}
+		}
+	}
+	for (int a = 24; a <= 37; a++)
+	{
+		move(69, a);
+		printf("|");
+
+		if (a == 37)
+		{
+			for (int b = 69; b <= 118; b++)
+			{
+				printf("-");
+			}
+		}
+	}
+
+	move(89, 30);
+	printf("게임 시작!");
+	move(85, 32);
+	printf("아무 키나 누르세요..");
+	printf("\n\n\n\n\n\n\n\n\n");
+	
+}
+void delete_print()//출력화면을 지운다.(공백으로 바꿈)
+{
+	for (int i = 0; i <= 100; i++)
+	{
+		FillConsoleOutputCharacter(GetStdHandle(STD_OUTPUT_HANDLE),' ', i * i, { 0, 0 }, &dw);//dw  =넓이를 반환할 곳.
+		Sleep(20);
+	}
+
+	check_function = true;
+	move(1, 1);
+
+}
+void print_omog()//오목판 출력
+{
+	//오목판은 15*15의 크기를 지녔다.
+
+	int omog_position_x = 82, omog_position_y = 15;//현재 나의 좌표. 키보드에 따라 좌표가 달라진다.
+
+	for (int a = 0; a <15; a++)//오목판 15*15 로 출력한다.
+	{
+		move(omog_position_x -4, omog_position_y +a);
+		printf("%2d",a);
+
+		for (int b = 0; b <15; b++)
+		{
+			move(omog_position_x + (b*2), omog_position_y + a);//x축은 2칸이 y축으로 1칸이다.
+			printf("□");
+		}
+	}
+	//오목칸의 넓이 x = 82~110, y = 15~29;
+
+	for (int a = 0; a < 15; a++)
+	{
+		move(omog_position_x +(a*2), 30);
+		printf("%c",a+65);
+	}
+
+
+
+
+
+
+
+}
+void omog_background()
+{
+	for (int a = 0; a <=110; a += 110)
+	{
+
+		move(8+a, 17);
+		printf("ㄴ");
+
+		move(12+a, 15);
+		printf("ㄱ");
+
+	}
+	
+}
+void omog_keyboard()
+{
+	int keyboard_value;
+	while (1)
+	{
+		keyboard_value = getch();
+		if (keyboard_value == 224)
+		{
+			keyboard_value = getch();
+			switch (keyboard_value)
+			{
+			case 72:
+				omog_position_y--;
+				break;
+
+			case 80:
+				omog_position_y++;
+				break;
+			case 75:
+				omog_position_x -= 2;
+				break;
+			case 77:
+				omog_position_x += 2;
+				break;
+
+
+
+
+			}
+			move(omog_position_x, omog_position_y);
+			printf("▤");
+		}
+
+		else if (keyboard_value == 13)
+		{
+			
+		}
+	}
+}
+
+
